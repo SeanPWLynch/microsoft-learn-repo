d8decd8a74c6873981139e672a050f382d157b7d
Sean Lynch <sean.p.w.lynch@gmail.com>
2021-02-25 20:04:06 +0000

    Added code from previous modules

---
+++ PhotoSharingApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Azure.Storage.Blobs;
+
+namespace PhotoSharingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                              .SetBasePath(Directory.GetCurrentDirectory())
+                              .AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+
+            var connectionString = configuration.GetConnectionString("StorageAccount");
+            string containerName = "photos";
+
+            BlobContainerClient container = new BlobContainerClient(connectionString, containerName);
+
+            container.CreateIfNotExists();
+
+            string blobName = "lisbon-chicken";
+            string fileName = "lisbon-chicken.jpeg";
+            BlobClient blobClient = container.GetBlobClient(blobName);
+            blobClient.Upload(fileName,true);
+
+            var blobs = container.GetBlobs();
+
+            foreach(var blob in blobs)
+            {
+                Console.WriteLine($"{blob.Name}  --> Created On: {blob.Properties.CreatedOn:yyyy-MM-dd HH:mm:ss} Size: {blob.Properties.ContentLength}");
+            }
+
+         }
+    }   
+}
