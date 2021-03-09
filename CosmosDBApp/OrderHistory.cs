f4515b1abc4c9d6beba406fcefcdf8c2e829a5ce
Sean Lynch <sean.p.w.lynch@gmail.com>
2021-03-09 21:40:54 +0000

    Added cosmosdb dotnet app

---
+++ CosmosDBApp/OrderHistory.cs
@@ -0,0 +1,9 @@
+namespace CosmosDBApp
+{
+    public class OrderHistory
+    {
+        public string OrderId { get; set; }
+        public string DateShipped { get; set; }
+        public string Total { get; set; }
+    }
+}
---
+++ CosmosDBApp/ShippingPreference.cs
@@ -0,0 +1,13 @@
+namespace CosmosDBApp
+{
+    public class ShippingPreference
+    {
+        public int Priority { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
+    }
+}
